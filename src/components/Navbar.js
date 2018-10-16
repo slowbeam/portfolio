66509e8fe34a8ed7c5773db35c3592e060e2fdc1
Sandy Edwards <sedwardscode@gmail.com>
2018-10-15 22:14:40 -0400

    built basic navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className='navbar'>
+        <div className='box-1'>
+          <h1>sandy edwards</h1>
+        </div>
+        <div className='box-2'>
+          <p>hamburger</p>
+        </div>
+      </div>
+    )
+  }
+};
+
+export default Navbar;
