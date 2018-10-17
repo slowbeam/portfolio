349811a130526db19edb2f417346221c349d89e0
Sandy <sedwardscode@gmail.com>
2018-10-17 17:45:48 -0400

    began building test scroller component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import AppCardContainer from './containers/AppCardContainer';
+import Scroller from './components/Scroller'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <AppCardContainer />
+        <Scroller />
       </div>
     );
   }
---
+++ src/components/Scroller.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Scroller extends React.Component {
+  render() {
+    return (
+      <div className='scroller'>
+        <div className='box-1'>
+          <h1>sandy edwards</h1>
+        </div>
+        <div className='box-2'>
+          <h1>hamburger</h1>
+        </div>
+        <div className='box-3'>
+          <h1>other</h1>
+        </div>
+        <div className='box-4'>
+          <h1>wowww</h1>
+        </div>
+      </div>
+    )
+  }
+};
+
+export default Scroller;
