b7af022dd60a47d0c71b3782bd431012ba913214
Sandy <sedwardscode@gmail.com>
2018-10-17 17:50:16 -0400

    added scroll functionality to scroller

--- src/components/Scroller.js
+++ src/components/Scroller.js
@@ -4,16 +4,16 @@ class Scroller extends React.Component {
   render() {
     return (
       <div className='scroller'>
-        <div className='box-1'>
+        <div className='page one'>
           <h1>sandy edwards</h1>
         </div>
-        <div className='box-2'>
+        <div className='page two'>
           <h1>hamburger</h1>
         </div>
-        <div className='box-3'>
+        <div className='page three'>
           <h1>other</h1>
         </div>
-        <div className='box-4'>
+        <div className='page four'>
           <h1>wowww</h1>
         </div>
       </div>
