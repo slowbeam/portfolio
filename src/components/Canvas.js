dd2ef411e661d02389fa7541ad242132dde1f6e1
Sandy <sedwardscode@gmail.com>
2018-10-18 18:20:28 -0400

    added canvas circle

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -6,6 +6,12 @@ class Canvas extends React.Component {
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext("2d");
     const img = this.refs.image;
+
+    img.onload = () => {
+      ctx.beginPath();
+      ctx.arc(100, 100, 100, 0, 2 * Math.PI)
+      ctx.stroke();
+    }
   }
 
   render() {
