319975fc50ebde0bb8a0928c7820c56aff26b216
Sandy <sedwardscode@gmail.com>
2018-10-18 18:36:08 -0400

    minor

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -4,20 +4,19 @@ class Canvas extends React.Component {
 
   componentDidMount() {
     const canvas = this.refs.canvas;
-    const ctx = canvas.getContext("2d");
+    const c = canvas.getContext("2d");
     const img = this.refs.image;
 
 
-    ctx.beginPath();
-    ctx.arc(100, 100, 100, 0, 2 * Math.PI)
-    ctx.stroke();
+    c.beginPath();
+    c.arc(100, 100, 100, 0, 2 * Math.PI)
+    c.stroke();
 
   }
 
   render() {
     return(
       <div className="canvas-container">
-        
         <canvas ref="canvas" width={640} height={425} />
       </div>
     )
