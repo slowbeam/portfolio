dc4edd52ccef1cbb2641bb2618fe959faf9c5fa2
Sandy <sedwardscode@gmail.com>
2018-10-19 20:04:17 -0400

    minor

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,14 +2,24 @@ import React from 'react';
 
 class Canvas extends React.Component {
 
+  drawCircles () {
+
+  }
+
   componentDidMount() {
     const canvas = this.refs.canvas;
     const c = canvas.getContext("2d");
-    const img = this.refs.image;
 
+    c.fillStyle = "rgba(255, 0 , 0, 0.5)"
+    c.fillRect(100, 100, 100, 100);
+    c.fillRect(400, 100, 100, 100);
+    c.fillRect(300, 300, 100, 100);
 
     c.beginPath();
-    c.arc(100, 100, 100, 0, 2 * Math.PI)
+    c.moveTo(50, 300);
+    c.lineTo(300, 100);
+    c.lineTo(400, 300);
+    c.strokeStyle = "#fa34a3";
     c.stroke();
 
   }
