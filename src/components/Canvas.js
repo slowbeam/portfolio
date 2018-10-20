be4c0cda6999af17b284e9773c8e10452f366185
Sandy <sedwardscode@gmail.com>
2018-10-19 20:37:01 -0400

    drawing random circles

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,38 +2,35 @@ import React from 'react';
 
 class Canvas extends React.Component {
 
-  drawCircles () {
+  drawRandomCircles () {
+    const canvas = this.refs.canvas;
+    const c = canvas.getContext("2d");
 
+    for (let i = 0; i < 3; i++) {
+      let x = Math.random() * (window.innerWidth * .8);
+      let y = Math.random() * (window.innerHeight * .7);
+
+      c.beginPath();
+      c.arc(x, y, 30, 0, Math.PI * 2);
+      c.strokeStyle = 'blue';
+      c.stroke();
+    }
   }
 
   componentDidMount() {
     const canvas = this.refs.canvas;
     const c = canvas.getContext("2d");
 
-    c.fillStyle = "rgba(255, 0 , 0, 0.5)";
-    c.fillRect(100, 100, 100, 100);
-    c.fillStyle = "rgba(0, 0 , 255, 0.5)";
-    c.fillRect(400, 100, 100, 100);
-    c.fillStyle = "rgba(0, 255, 0, 0.5)";
-    c.fillRect(300, 300, 100, 100);
-    c.fillStyle = "#000";
-    c.fillRect(400, 200, 200, 200);
-
-    c.beginPath();
-    c.moveTo(50, 300);
-    c.lineTo(300, 100);
-    c.lineTo(400, 300);
-    c.lineTo(550, 50);
-    c.strokeStyle = "#f34a35";
-    c.stroke();
-
+    this.drawRandomCircles();
     // Arc / Circle
-    c.beginPath();
-    c.arc(300, 300, 30, 0, Math.PI * 2);
-    c.strokeStyle = 'blue';
-    c.stroke();
+    // c.beginPath();
+    // c.arc(300, 300, 30, 0, Math.PI * 2);
+    // c.strokeStyle = 'blue';
+    // c.stroke();
 
   }
+
+
 
   render() {
     return(
