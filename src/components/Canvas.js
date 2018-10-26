4251d0ac67897612977aa42b1402a39ddd6dbb0d
Sandy <sedwardscode@gmail.com>
2018-10-25 21:40:20 -0400

    attempting to animate image on canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -66,7 +66,7 @@ class Canvas extends React.Component {
       }
 
       this.update = function() {
-        if (this.x + this.radius >canvas.width || this.x - this.radius < 0) {
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
           this.dx = -this.dx;
         }
 
@@ -95,6 +95,20 @@ class Canvas extends React.Component {
       return circleArray;
     }
 
+    let imageArray = [];
+
+    for (let i = 0; i < 100; i++) {
+      let width = 30;
+      let height = 30;
+      let url = "https://banner2.kisspng.com/20180422/thq/kisspng-guy-fieri-food-network-tv-personality-chef-restaur-guy-5add3af0e59aa7.0049169215244479849405.jpg"
+      let x = Math.random() * (canvas.width - width * 2) + width;
+      let y = Math.random() * (canvas.height - height * 2) + height;
+      let dx = (Math.random() - 0.5) * 2;
+      let dy = (Math.random() - 0.5) * 2;
+
+      imageArray.push(new Image(x, y, dx, dy, width, height, url))
+    }
+
     const circleArrayOne = createCircleArray("rgba(50, 50, 100, .6)");
     const circleArrayTwo = createCircleArray("rgba(50, 100, 100, .2)");
     const animate = () => {
@@ -102,26 +116,90 @@ class Canvas extends React.Component {
       c.clearRect(0, 0, canvas.width, canvas.height);
       requestAnimationFrame(animate);
 
-      for (let i = 0; i < circleArrayOne.length; i++) {
-        circleArrayOne[i].update();
-      }
-      for (let i = 0; i < circleArrayTwo.length; i++) {
-        circleArrayTwo[i].update();
-      }
+      // for (let i = 0; i < circleArrayOne.length; i++) {
+      //   circleArrayOne[i].update();
+      // }
+      // for (let i = 0; i < circleArrayTwo.length; i++) {
+      //   circleArrayTwo[i].update();
+      // }
+      // for (let i = 0; i < imageArray.length; i++) {
+      //   imageArray[i].update();
+      // }
+
     }
     animate();
     }
 
+  drawGuy() {
+
+    const canvas = this.refs.canvas;
+    const c = canvas.getContext("2d");
+
+    function Guy(x, y, dx, dy, width, height, url) {
+      this.x = x;
+      this.y = y;
+      this.dx = dx;
+      this.dy = dy;
+      this.width = width;
+      this.height = height;
+      this.url = url;
+
+      this.draw = function() {
+        let base_image = new Image();
+        base_image.src = this.url;
+        base_image.onload = function() {
+          c.drawImage(base_image, x, y, this.width, this.height)
+        }
+      }
+
+      this.update = function() {
+        if (this.x + this.width / 2 > canvas.width || this.x - this.height / 2 < 0) {
+          this.dx = -this.dx;
+        }
+
+        if (this.y + this.height / 2 > canvas.height || this.y - this.height / 2 < 0) {
+          this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+        this.draw()
+      }
+    }
+
+
+
+    let width = 30;
+    let height = 30;
+    let url = "https://banner2.kisspng.com/20180422/thq/kisspng-guy-fieri-food-network-tv-personality-chef-restaur-guy-5add3af0e59aa7.0049169215244479849405.jpg"
+    let x = Math.random() * (canvas.width - width * 2) + width;
+    let y = Math.random() * (canvas.height - height * 2) + height;
+    let dx = (Math.random() - 0.5) * 2;
+    let dy = (Math.random() - 0.5) * 2;
+
+    let image = new Guy(30, 50, dx, dy, 50, 80, url);
+    image.draw()
+  }
 
   componentDidMount() {
 
+    const canvas = this.refs.canvas;
+    const c = canvas.getContext("2d");
 
+    let width = 30;
+    let height = 30;
+    let url = "https://png.pngtree.com/element_pic/16/06/03/095750dcf5985d0.jpg"
+
+    let base_image = new Image();
+    base_image.src = this.url;
+    base_image.onload = function() {
+    c.drawImage(base_image, 50, 50)
+    }
     // this.drawRandomCircles();
     // this.drawRandomRectangles();
     // Arc / Circle
 
     this.runAnimation();
-
+    this.drawGuy();
 
   }
 
