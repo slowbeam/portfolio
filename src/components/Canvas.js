36bfccb6947356ede55a1d5f2eb6a8b2c9ce87fd
slowbeam <sedwardscode@gmail.com>
2018-11-25 21:41:56 -0500

    removed unnecessary methods in canvas component

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -15,41 +15,6 @@ class Canvas extends React.Component {
     canvas.setAttribute('width', style_width * dpi);
   }
 
-
-  drawRandomCircles () {
-    const canvas = this.refs.canvas;
-    const c = canvas.getContext("2d");
-
-    const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-
-    for (let i = 0; i < 400; i++) {
-      let x = Math.random() * (window.innerWidth * .89);
-      let y = Math.random() * (window.innerHeight * .79);
-      let colorNum = Math.floor(Math.random() * 8);
-
-      c.beginPath();
-      c.arc(x, y, 30, 0, Math.PI * 2);
-      c.strokeStyle = colors[colorNum];
-      c.stroke();
-    }
-  }
-
-  drawRandomRectangles() {
-
-    const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-
-    const canvas = this.refs.canvas;
-    const c = canvas.getContext("2d");
-
-    for (let i = 0; i < 100; i++) {
-      let x = Math.random() * (window.innerWidth * .89);
-      let y = Math.random() * (window.innerHeight * .79);
-      let colorNum = Math.floor(Math.random() * 8);
-      c.fillStyle = colors[colorNum];
-      c.fillRect(x, y, 50, 50);
-    }
-  }
-
   rectangleWaterfall = () => {
     const canvas = this.refs.canvas;
     const c = canvas.getContext("2d");
