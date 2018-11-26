281d951d8ca4c441cb073f2f18b117c76b822395
slowbeam <sedwardscode@gmail.com>
2018-11-25 21:54:05 -0500

    began adding triangle background element to app

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="triangle-topleft">
+        </div>
         <Scroller />
       </div>
     );
