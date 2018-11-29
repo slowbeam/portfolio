8a8c275261ffb6dbafc1f5bae8ffa4a5daedf6f8
Sandy <sedwardscode@gmail.com>
2018-11-28 19:01:00 -0500

    made title div polygon and added projects button div

--- src/components/Scroller.js
+++ src/components/Scroller.js
@@ -1,45 +1,46 @@
-import React from 'react';
-import Canvas from './Canvas';
+import React from "react";
+import Canvas from "./Canvas";
 
 class Scroller extends React.Component {
   render() {
     return (
-      <div className='scroller'>
-        <div className='page five'>
+      <div className="scroller">
+        <div className="page five">
           <div className="inside five">
-            <div className='container'>
+            <div className="container">
               <Canvas />
               <div className="overlay">
-                <div className="intro-text-container" >
-                  <h1>Hi, I'm Sandy Edwards.
-                  <br />
-                  I'm a full-stack developer.
+                <div className="intro-text-container">
+                  <h1>
+                    Hi, I'm Sandy Edwards.
+                    <br />
+                    I'm a full-stack developer.
                   </h1>
                 </div>
               </div>
+              <div className="projects-button">projects</div>
             </div>
-
           </div>
         </div>
 
-        <div className='page one'>
+        <div className="page one">
           <div className="inside one">
             <h1>here are my projects</h1>
           </div>
         </div>
-        <div className='page two'>
+        <div className="page two">
           <div className="inside two">
             <h1>here are my blog posts</h1>
           </div>
         </div>
-        <div className='page three'>
+        <div className="page three">
           <div className="inside three">
-              <h1>contact me here</h1>
+            <h1>contact me here</h1>
           </div>
         </div>
       </div>
-    )
+    );
   }
-};
+}
 
 export default Scroller;
