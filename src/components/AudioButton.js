2cedb103353bb781a615c78ed5671ac511b6853d
slowbeam <sedwardscode@gmail.com>
2018-12-03 23:23:44 -0500

    added toggle functionality for audio play button

--- src/components/AudioButton.js
+++ src/components/AudioButton.js
@@ -2,15 +2,24 @@ import React from 'react';
 
 class AudioButton extends React.Component {
 
-  handleClick = () => {
-    const audio = new Audio(require('../audio/dp-gl-8bit.mp3'));
-    audio.play();
-  };
+  constructor(props) {
+    super(props);
+    this.state = { play: false };
+    this.url = require('../audio/dp-gl-8bit.mp3');
+    this.audio = new Audio(this.url);
+    this.togglePlay = this.togglePlay.bind(this);
+  }
+
+  togglePlay = () => {
+   this.setState({ play: !this.state.play }, () => {
+     this.state.play ? this.audio.play() : this.audio.pause();
+   });
+ }
 
   render() {
     return (
-      <div onClick={this.handleClick} className="audio-button">
-        <i className="fas fa-play"></i>
+      <div onClick={this.togglePlay} className="audio-button">
+        {this.state.play ? <i className="fas fa-pause"></i> : <i className="fas fa-play"></i>}
       </div>
     )
   }
