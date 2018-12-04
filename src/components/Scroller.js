29d884b44411843948e91ca7c94e11564cb0ce58
slowbeam <sedwardscode@gmail.com>
2018-12-03 21:50:10 -0500

    doubled size of projects div but still having issues with overlapping second and third page

--- src/components/Scroller.js
+++ src/components/Scroller.js
@@ -6,8 +6,8 @@ class Scroller extends React.Component {
   render() {
     return (
       <div className="scroller">
-        <div className="page five">
-          <div className="inside five">
+        <div className="page one">
+          <div className="inside one">
             <div className="container">
               <Canvas />
               <div className="overlay">
@@ -25,19 +25,19 @@ class Scroller extends React.Component {
           </div>
         </div>
 
-        <div className="page one">
-          <div className="inside one">
-            <h1>here are my projects</h1>
-            <AppCardContainer />
-          </div>
-        </div>
         <div className="page two">
           <div className="inside two">
-            <h1>here are my blog posts</h1>
+            <h1>projects</h1>
+            <AppCardContainer />
           </div>
         </div>
         <div className="page three">
           <div className="inside three">
+            <h1>here are my blog posts</h1>
+          </div>
+        </div>
+        <div className="page four">
+          <div className="inside four">
             <h1>contact me here</h1>
           </div>
         </div>
