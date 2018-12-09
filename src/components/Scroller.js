71c6e6cb7b9b0b7358b2e222f7051d09083722f0
Sandy <sedwardscode@gmail.com>
2018-12-09 13:20:44 -0500

    altered shape of blog button

--- src/components/Scroller.js
+++ src/components/Scroller.js
@@ -24,7 +24,9 @@ class Scroller extends React.Component {
                 <a href="#projects-page">projects</a>
               </div>
               <div className="about-button">about</div>
-              <div className="blog-button">blog</div>
+              <div className="blog-button">
+                <div id="blog-button-text">blog</div>
+              </div>
             </div>
           </div>
         </div>
