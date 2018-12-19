67257c912bf3adea51b0b0a49953a63d0882a03b
Sandy <sedwardscode@gmail.com>
2018-12-19 15:11:09 -0500

    added scroll function to smoothly scroll to target div when menu button is clicked

--- src/components/Scroller.js
+++ src/components/Scroller.js
@@ -3,6 +3,40 @@ import Canvas from "./Canvas";
 import Footer from "./Footer";
 
 class Scroller extends React.Component {
+  smoothScroll = target => {
+    let scrollContainer = target;
+    do {
+      scrollContainer = scrollContainer.parentNode;
+      if (!scrollContainer) {
+        return;
+      }
+      scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop === 0);
+
+    let targetY = 0;
+    do {
+      if (target === scrollContainer) break;
+      targetY += target.offsetTop;
+    } while ((target = target.offsetParent));
+
+    const startScroll = function(c, a, b, i) {
+      i++;
+      if (i > 30) return;
+      c.scrollTop = a + ((b - a) / 30) * i;
+      setTimeout(function() {
+        startScroll(c, a, b, i);
+      }, 20);
+    };
+
+    startScroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+  };
+
+  handleMenuClick = targetClass => {
+    const page = document.querySelector(targetClass);
+    console.log(page);
+    this.smoothScroll(page);
+  };
+
   render() {
     return (
       <div className="scroller">
@@ -22,8 +56,11 @@ class Scroller extends React.Component {
                 </div>
               </div>
               <div id="projects-button-container">
-                <div className="projects-button">
-                  <a href="#projects-page">projects</a>
+                <div
+                  className="projects-button"
+                  onClick={() => this.handleMenuClick(".page.two")}
+                >
+                  <a href="#">projects</a>
                 </div>
               </div>
               <div id="about-button-shadow-container">
@@ -64,33 +101,26 @@ class Scroller extends React.Component {
             <div className="projects-container">
               <div className="project-container">
                 <div className="project-title-container">
-                  <div className="project-title-left-polygon"></div>
-                  <div className="project-title-text">
-                    vibelist
-                  </div>
-                  <div className="project-title-right-polygon"></div>
+                  <div className="project-title-left-polygon" />
+                  <div className="project-title-text">vibelist</div>
+                  <div className="project-title-right-polygon" />
                 </div>
               </div>
               <div className="project-container">
                 <div className="project-title-container-two">
-                  <div className="project-title-left-polygon"></div>
-                  <div className="project-title-text">
-                    birthify
-                  </div>
-                  <div className="project-title-right-polygon"></div>
+                  <div className="project-title-left-polygon" />
+                  <div className="project-title-text">birthify</div>
+                  <div className="project-title-right-polygon" />
                 </div>
               </div>
               <div className="project-container">
                 <div className="project-title-container-three">
-                  <div className="project-title-left-polygon"></div>
-                  <div className="project-title-text">
-                    beat-machine
-                  </div>
-                  <div className="project-title-right-polygon"></div>
+                  <div className="project-title-left-polygon" />
+                  <div className="project-title-text">beat-machine</div>
+                  <div className="project-title-right-polygon" />
                 </div>
               </div>
             </div>
-
           </div>
         </div>
         <div id="blog-page" className="page three">
