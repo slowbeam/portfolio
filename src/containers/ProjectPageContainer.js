21f6c87ba8194a311948bcfa333e5942c5402212
Sandy <sedwardscode@gmail.com>
2019-01-11 11:34:53 -0500

    added all project info to state of project page container

--- src/containers/ProjectPageContainer.js
+++ src/containers/ProjectPageContainer.js
@@ -22,8 +22,17 @@ class ProjectPageContainer extends React.Component {
         images: ["../img/bf-2.gif"],
         description: "Birthify",
         links: {
-          Github: "https://github.com/slowbeam/vibe-list-client",
-          "Live App": "https://vibelist.herokuapp.com/"
+          Github: "https://github.com/slowbeam/birthify-client",
+          Demo: "https://www.youtube.com/watch?v=OsuEvbesbe4&feature=youtu.be"
+        }
+      },
+      {
+        name: "Beat-Machine",
+        images: ["../img/bm-1.gif", "../img/bm-2.gif", "../img/bm-3.gif"],
+        description: "Beat-Machine",
+        links: {
+          Github: "https://github.com/slowbeam/beat-machine-app",
+          "Live App": "https://beat-machine.com/"
         }
       }
     ]
