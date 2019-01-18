bf6a4aae2b82571fa1bfef252bb733d91c85af25
Sandy <sedwardscode@gmail.com>
2019-01-18 17:20:02 -0500

    styled email button

--- src/components/ContactButton.js
+++ src/components/ContactButton.js
@@ -4,7 +4,7 @@ class ContactButton extends React.Component {
   render() {
     return (
       <div className="email-button">
-        <a href="mailto:sedwardscode@gmai.com">Drop Me a Line</a>
+        <a href="mailto:sedwardscode@gmail.com">Drop Me a Line</a>
       </div>
     );
   }
