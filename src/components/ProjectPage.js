5f06e68059c8b828536fd55ee659b271e432cc9a
Sandy <sedwardscode@gmail.com>
2019-01-18 16:28:13 -0500

    made project page description para div narrower

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -31,26 +31,24 @@ class ProjectPage extends React.Component {
           <div className="project-title-right-polygon" />
         </div>
         <div className="project-content">
-
-        <div className="project-page-description">
-          <div className="project-page-description-paragraph">
-            {this.props.description}
+          <div className="project-page-description">
+            <div className="project-page-description-paragraph">
+              {this.props.description}
+            </div>
+            <div className="project-links-container">
+              <a href="https://github.com/slowbeam/vibe-list-client">
+                {Object.keys(this.props.links)[0]}
+              </a>
+              &nbsp; | &nbsp;
+              <a href="https://vibelist.herokuapp.com/">
+                {Object.keys(this.props.links)[1]}
+              </a>
+            </div>
           </div>
-          <div className="project-links-container">
-            <a href="https://github.com/slowbeam/vibe-list-client">
-              {Object.keys(this.props.links)[0]}
-            </a>
-            &nbsp; | &nbsp;
-            <a href="https://vibelist.herokuapp.com/">
-              {Object.keys(this.props.links)[1]}
-            </a>
+          <div className="project-page-image-container">
+            {this.renderProjectImages()}
           </div>
         </div>
-        <div className="project-page-image-container">
-          {this.renderProjectImages()}
-        </div>
-
-      </div>
       </div>
     );
   }
--- src/containers/ProjectPageContainer.js
+++ src/containers/ProjectPageContainer.js
@@ -30,9 +30,7 @@ class ProjectPageContainer extends React.Component {
       },
       {
         name: "Beat-Machine",
-        images: [
-          images.beatMachineGif1
-        ],
+        images: [images.beatMachineGif1],
         description:
           "Beat-Machine is an online music making tool inspired by the famous 1980's drum machine the Roland TR-808. Users can create and save complex drum beats, select instrument packs for their beat and adjust the tempo and swing of the beat.",
         links: {
