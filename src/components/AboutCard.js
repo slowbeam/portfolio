a58090b4498e6b09b127f87deecb970b5cf33663
Sandy <sedwardscode@gmail.com>
2019-01-23 09:51:40 -0500

    styled text on about card more

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -13,19 +13,22 @@ class AboutCard extends React.Component {
         </div>
 
         <div className="about-description">
-          <p>
+          <p id="about-headline">
             I'm an artist, musician and software engineer and I love combining
             my three passions to create amazing products.
           </p>
           <p>
-            I'm highly skilled in JavaScript, React, Redux, Thunk, HTML5, CSS3,
-            SASS, Ruby, Ruby on Rails, Rspec, SQL and PostgreSQL.
+            I'm a recent graduate of the full-stack software engineering
+            immersive at the Flatiron School in NYC, and I'm currently searching
+            for the perfect fit in a role to start my career. However, my french
+            bulldog Jellybean would prefer it if I was a stay-at-home dad
+            forever.
           </p>
+          <h5 id="technical-skills-title">Technical Skills:</h5>
           <p>
-            I attended the full-stack software engineering immersive at the
-            Flatiron School in NYC, and I'm currently searching for the perfect
-            fit in a role to start my career. However, my french bulldog
-            Jellybean would prefer it if I was a stay-at-home dad forever.
+            JavaScript, React, Redux, Thunk, HTML5, CSS3, SASS, Ruby, Ruby on
+            Rails, Rspec, SQL, PostgreSQL, Adobe Illustrator, Photoshop and
+            Indesign.
           </p>
         </div>
       </div>
