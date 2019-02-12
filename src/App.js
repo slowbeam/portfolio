47fe216554462cfea4d6d2fd750b293c40c6d69d
slowbeam <sedwardscode@gmail.com>
2019-02-12 18:00:14 -0500

    added preload script to force css transitions to wage for page load

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import "./App.css";
 import Scroller from "./components/Scroller";
 
 class App extends Component {
+
+  componentDidMount() {
+    window.addEventListener('load', this.handlePageLoad);
+  }
+
+  handlePageLoad = () => {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove("preload");
+  }
+
   render() {
     return (
       <div className="App">
